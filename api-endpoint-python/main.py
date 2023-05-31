2e8678b48bb32ea107721f085f26f21e5551fd3f
Andhika R <dhikramadhan008@gmail.com>
2023-06-01 06:10:32 +0700

    update predict

--- api-endpoint-python/main.py
+++ api-endpoint-python/main.py
@@ -10,18 +10,31 @@ import base64
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.regularizers import l2
+from tensorflow.keras import regularizers
 from flaskext.mysql import MySQL
 import io
 
 
-# classPred = ['incorrect_mask', 'with_mask', 'without_mask']
+img_size = (224, 224)
+channels = 3
+img_shape = (img_size[0], img_size[1], channels)
+
+base_model = tf.keras.applications.efficientnet.EfficientNetB3(include_top= False, weights= "imagenet", input_shape= img_shape, pooling= 'max')
+
+def make_model():
+    model = tf.keras.models.Sequential([
+        base_model,
+        tf.keras.layers.BatchNormalization(axis= -1, momentum= 0.99, epsilon= 0.001),
+        tf.keras.layers.Dense(256, kernel_regularizer= regularizers.l2(l= 0.016), activity_regularizer= regularizers.l1(0.006),
+                bias_regularizer= regularizers.l1(0.006), activation= 'relu'),
+        tf.keras.layers.Dropout(rate= 0.45, seed= 123),
+        tf.keras.layers.Dense(3, activation= 'softmax')
+    ])
+    return model
 
 bucket_name = "skinnie-bucket"
 
 # model = None
-
-
 
 app = Flask(__name__)
 
@@ -35,10 +48,30 @@ app.config['MYSQL_DATABASE_HOST'] = '34.128.86.191'
 
 mysql.init_app(app)
 
-#ini yang baru ka
+def download_model():
+    model_file = "models/efficientnetb3_98.h5"  # Replace with your model file name
+    weights_file = "models/efficientnetb3_98_weights.h5"  # Replace with your weights file name
+    
+    destination_path = "/tmp/efficientnetb3_98.h5"  # Path to store the downloaded model file
+    weights_path = "/tmp/efficientnetb3_98_weights.h5"  # Path to store the downloaded weights file
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    model_blob = bucket.blob(model_file)
+    weights_blob = bucket.blob(weights_file)
+    model_blob.download_to_filename(destination_path)
+    weights_blob.download_to_filename(weights_path)
+
+def load_model():
+    global model
+    global weights_file_path
+    if not os.path.isfile("/tmp/efficientnetb3_98.h5") or not os.path.isfile("/tmp/efficientnetb3_98_weights.h5"):
+        download_model()
+    model = make_model()  # Buat model terlebih dahulu
+    model.load_weights("/tmp/efficientnetb3_98_weights.h5")
+
 def predict_image(image_path):
-    model = load_model('model1.h5')
-    model.load_weights('model1-weights.h5')
+    load_model()
     img = image.load_img(image_path, target_size=(224, 224))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
@@ -49,14 +82,13 @@ def predict_image(image_path):
     predicted = classes[np.argmax(pred)]
     return predicted_class, predicted
 
-#=============================================================================
-#ini yang baru ka
 @app.route('/predict', methods=['POST'])
 def predict():
     file = request.files['image']
     file.save('uploaded_image.jpg')
     predicted_class = predict_image('uploaded_image.jpg')
     return jsonify({'prediction': predicted_class})
+
 
 @app.route('/register', methods=['POST'])
 def register():
