1f8b686fc56c8e9f3c4550e01b41dbbb9b7e3e0a
SkinnieApp <134658004+skinnie-project@users.noreply.github.com>
2023-06-04 18:24:17 +0700

    tambah artikel

---
+++ product/article.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+# Konfigurasi koneksi MySQL
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'skinnie-db-project'
+app.config['MYSQL_DATABASE_DB'] = 'skinnie_database'
+app.config['MYSQL_DATABASE_HOST'] = '34.128.86.191'
+
+mysql.init_app(app)
+
+def get_article():
+    article_id = request.args.get('id')
+
+    # Membuat koneksi MySQL
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM artikel WHERE id = '" + article_id + "'"
+    cursor.execute(query)
+    
+    try:
+        # Mendapatkan hasil query
+        rows = cursor.fetchall()
+        results = []
+        for row in rows:
+            result = {
+                'id': row[0],
+                'ditinjau': row[1],
+                'tanggal': row[2],
+                'judul': row[3],
+                'sumber': row[4]
+            }
+            results.append(result)
+
+        # Mengembalikan hasil dalam format JSON
+        return jsonify(results)
+
+    except Exception as e:
+        conn.close()
+        response = {
+            'status': 'error',
+            'message': 'Terjadi kesalahan saat mengambil data',
+            'error': str(e)
+        }
+        return jsonify(response)
